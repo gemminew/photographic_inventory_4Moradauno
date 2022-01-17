cf104f41cd90951453ede9664b062c22f9137ddd
Susana Garcia <susanlevart@gmail.com>
2022-01-17 09:16:36 -0600

    Añadiendo Login & Render

---
+++ src/Login/Login.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+        <H1> Ingresa aqui </H1>
+    <form action="">
+      <h2>Nombre</h2>
+      <input type="text" />
+      <h2>Correo Electronico</h2>
+      <input type="text" />
+      <h2>Inmobiliaria</h2>
+      <input type="text" />
+      <Button> Iniciar Sesion </Button>
+    </form>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/RenderArchives/RenderArchives.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const RenderArchives = () => {
+  return (
+    <div>
+    <h1> Registros </h1>
+    <div>
+  <img src="Instagram.png" alt="" />
+  <img src="Instagram.png" alt="" />
+  <img src="Instagram.png" alt="" />
+  <img src="Instagram.png" alt="" />
+  <img src="Instagram.png" alt="" />
+  <img src="Instagram.png" alt="" />
+  <img src="Instagram.png" alt="" />
+    </div>
+    </div>
+  );
+}
+
+export default RenderArchives;
