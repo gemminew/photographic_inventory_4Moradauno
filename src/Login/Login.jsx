60071730591d66339d21bbd44c60de42d4ad6065
Susana Garcia <susanlevart@gmail.com>
2022-01-17 17:47:43 -0600

    Login&Render
    Subiendo nuevos cambios de Login y Render

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -3,16 +3,20 @@ import React from 'react';
 const Login = () => {
   return (
     <div>
-        <H1> Ingresa aqui </H1>
+      <div>
+        <h1> Ingresa aqui </h1>
     <form action="">
       <h2>Nombre</h2>
-      <input type="text" />
-      <h2>Correo Electronico</h2>
-      <input type="text" />
+      <input type="text" placeholder = " Susana Garcia " />
+      <h2 >Correo Electronico</h2>
+      <input type="text"  placeholder="ej. example@gmail.com"/>
       <h2>Inmobiliaria</h2>
-      <input type="text" />
-      <Button> Iniciar Sesion </Button>
+      <input type="text" placeholder = "ej. Mi casa " />
+      <h2 className ="text-login" > Contraseña </h2>
+     <input className ="input-login" id="contraseña" type="password" name="password" placeholder="**********"/>
+      <button> Iniciar Sesion </button>
     </form>
+    </div>
     </div>
   );
 }
--- src/RenderArchives/RenderArchives.jsx
+++ src/RenderArchives/RenderArchives.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import comedor from "../Assets/comedor.jpg";
 
 const RenderArchives = () => {
   return (
     <div>
-    <h1> Registros </h1>
+    <h1> Galeria - 18723912 </h1>
     <div>
-  <img src="Instagram.png" alt="" />
-  <img src="Instagram.png" alt="" />
-  <img src="Instagram.png" alt="" />
-  <img src="Instagram.png" alt="" />
-  <img src="Instagram.png" alt="" />
-  <img src="Instagram.png" alt="" />
-  <img src="Instagram.png" alt="" />
+  <img id="img-logo" src={comedor} alt="img" className="logo-img" with ="100px" height="70"/>
+  <h3>Lorem ipsum dolor sit amet consectetur adipiscing elit tristique suscipit, mus pharetra nostra nisl lacinia senectus lacus quisque.</h3>
     </div>
+    <div>
+    <button> Regresar </button>
+    <button> Cerrar Sesion </button>
+    </div>
+   
     </div>
   );
 }
