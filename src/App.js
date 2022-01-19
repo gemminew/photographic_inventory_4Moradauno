0b79e3633f8d369cc900047b1b39a887f2cb4a51
RocioNi <rcno87@gmail.com>
2022-01-18 22:45:01 -0600

    diseño header

--- src/App.js
+++ src/App.js
@@ -6,7 +6,16 @@ import './App.css';
 function App() {
   return (
     <FilesProvider>
-      <h1>Morada uno</h1>
+       <div id="containerHeader">
+         <h1><img src="images/moradalogo.png" alt="moradalogo" width="40%"/>
+         {/* <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300&display=swap" rel="stylesheet"></link> */}
+      <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;400&display=swap" rel="stylesheet"></link>
+         <button className="LogOut" type="submit" >Cerrar sesión</button>
+          </h1>
+       </div>
+       <body>
+       <button className="addItem" type="submit" >Añadir nuevo registro</button>
+       </body>
     </FilesProvider>
   );
 }
