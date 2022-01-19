d23e40f16a075fb80604ec1b05ef690eb8c47e48
RocioNi <rcno87@gmail.com>
2022-01-19 01:30:07 -0600

    Diseño2

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ import './App.css';
 function App() {
   return (
     <FilesProvider>
+      <div id= "allContainer">
        <div id="containerHeader">
-         <h1><img src="images/moradalogo.png" alt="moradalogo" width="40%"/>
+         <img src="images/moradalogo.png" alt="moradalogo" width="40%"/>
+         <button className="LogOut" type="submit" >Cerrar sesión</button>
          {/* <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300&display=swap" rel="stylesheet"></link> */}
       <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;400&display=swap" rel="stylesheet"></link>
-         <button className="LogOut" type="submit" >Cerrar sesión</button>
-          </h1>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0,minimum-scale=1.0" />
        </div>
        <body>
+         <br></br>
        <button className="addItem" type="submit" >Añadir nuevo registro</button>
        </body>
+       </div>
     </FilesProvider>
   );
 }
