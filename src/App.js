f53d25f57bc545d197e4e20dc8ebc2fad08476cc
Gemma <gemminew@gmail.com>
2022-01-20 13:54:37 -0600

    header

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, {useState} from 'react'
 import Login from './components/Login/Login'
 import  FilesProvider  from './Context/filesContext'
 import Stepper from './components/Stepper';
-import Main from './components/Main/Main'
+import PersistentDrawerRight from './components/Main/Main'
 
 
 function App() {
- const [user, setUser] = useState(null);
+ //const [user, setUser] = useState(null);
 
   return (
     <FilesProvider>
-      {user ? <Stepper/> : <Login />}      
+      <PersistentDrawerRight></PersistentDrawerRight>
       <h1>Morada uno</h1>
     </FilesProvider>
   );
