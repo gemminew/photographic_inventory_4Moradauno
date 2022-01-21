3982d897da13eaab5bade0ffef12db534f1b0488
Susana Garcia <susanlevart@gmail.com>
2022-01-21 15:05:01 -0600

    vista del centro del stepper
    Cambio  por revisar

---
+++ src/components/UploadFiles/ImgUp.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import LabelBottomNavigation from './LabelBottomNavigation';
+import './ImgUp.css'
+
+export default function ImgUp () {
+  return (
+    <div id = "div2">
+    <h1 id='comentarios' >Cometarios </h1>
+    <input id='placeComent' type="text" placeholder='Agrega aqui tu comentario' />
+    <h1 id='categoria'>Categorias  </h1>
+    <LabelBottomNavigation/>
+    <h1 id='imagenes'>Imagenes</h1>
+    <input id='imgUp' type="file" accept="image/*" capture="camera"></input>
+    <div id='divBoton'>
+    <button id='guardar'>Guardar</button>
+    <button id='enviar'>Enviar</button>
+    </div>
+    
+    </div>
+  );
+}
